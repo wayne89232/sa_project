f913173a624ab0fc1c0f4deeda9bf12c24f3366b
DSW <dsw41923@gmail.com>
2015-06-07 14:49:36 +0800

    some revise including indent

--- models/user.js
+++ models/user.js
@@ -1,15 +1,15 @@
 exports.User = function(Sequelize, sequelize){
-  return sequelize.define('User', {
-    player_id: { type: Sequelize.INTEGER, primaryKey: true }, 
-    photo_url: Sequelize.STRING ,
-    user_name: Sequelize.STRING,
-    birthdate: Sequelize.STRING,
-    email: Sequelize.STRING, 
-    user_type: {
-    	type:   Sequelize.ENUM,
-    	values: ['admin', 'donor']
-    }
-  },{
-    tableName: 'User'
-  });
+	return sequelize.define('User', {
+		player_id: { type: Sequelize.INTEGER, primaryKey: true }, 
+		photo_url: Sequelize.STRING ,
+		user_name: Sequelize.STRING,
+		birthdate: Sequelize.STRING,
+		email: Sequelize.STRING, 
+		user_type: {
+			type:   Sequelize.ENUM,
+			values: ['admin', 'donor']
+		}
+	},{
+		tableName: 'User'
+	});
 }
