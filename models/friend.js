d7ff79d479610b1fabb10187ea9c6556aed57770
DSW <dsw41923@gmail.com>
2015-06-10 20:53:29 +0800

    Temp Done

--- models/friend.js
+++
@@ -1,9 +0,0 @@
-exports.Friend = function(Sequelize, sequelize){
-	return sequelize.define('Friend', {
-		id: { type: Sequelize.INTEGER, primaryKey: true },
-		user_id: Sequelize.INTEGER,
-		friend_id: Sequelize.INTEGER
-	},{
-		tableName: 'Friend'
-	});
-}
