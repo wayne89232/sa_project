ef604d2e08b7d6d1dbc4789b5a4e107f0a357dda
wei2 <b01705015@ntu.edu.tw>
2015-06-10 12:20:14 +0800

    login

--- routes/api.js
+++ routes/api.js
@@ -4,4 +4,36 @@
 var _ = require('underscore');
 
 
+exports.checkLogin = function(req, res, next){
+	var isLogin = false;
+	if( _.has(req.session, 'isLogin') ){
+		isLogin = req.session.isLogin;
+	}
+	if(isLogin){
+		next();
+	}else{
+		res.status(401).json({
+			error:true,
+			msg:"Plz login"
+		});
+	}
+}
 
+exports.login = function (req, res){
+	var query = {
+		where:{
+			account: req.body.account
+		}
+	}
+	User.find(query).success(function(user){
+		if(user == null){
+			res.json({msg:"No user!"});
+		}
+		else{
+			var user = _.omit(user.dataValues, 'password', 'createdAt', 'updatedAt');
+			req.session.user = user;
+			req.session.isLogin = true;
+			res.json({msg:"login success"});
+		}
+	});
+}
